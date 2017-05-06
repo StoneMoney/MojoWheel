2013fe7f8c1bdc6a7f56df05777dcedd2c230863
stonemoney <nickyg515@yahoo.com>
2017-05-05 21:07:34 -0400

    fixes

--- server.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd C:\Users\Jason\Documents\MojoWheel\
-node server.js
-start chrome --app="file:///C:/Users/Jason/Documents/MojoWheel/index.html/" --window-size=516,516 --window-position=0,0 
